9336fd13328d27119147368f284d32289cd0251e
Doles23 <dolewski.patryk@wp.pl>
2021-09-13 15:35:21 +0200

    wserfd

--- helloword.js
+++ helloword.js
@@ -9,3 +9,23 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
 });
+
+const square = require('./square'); // Here we require() the name of the file without the (optional) .js file extension
+console.log('The area of a square with a width of 4 is ' + square.area(4));
+
+
+console.log('First');
+console.log('Second');
+setTimeout(function() {
+    console.log('First');
+    }, 3000);
+ console.log('Second');
+
+
+ app.all('/secret', function(req, res, next) {
+  console.log('Accessing the secret section ...');
+  next(); // pass control to the next handler
+});
+const wiki = require('./wiki.js');
+// ...
+app.use('/wiki', wiki);
